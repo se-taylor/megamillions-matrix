13112df235d5fefc4cc2606b5719abfe1b5f6ad7
se-taylor <102394171+se-taylor@users.noreply.github.com>
2022-05-14 20:55:49 -0500

    Number Frequency Updates
    Added Days Ago and Last Drawn date information per number/drawn line item

--- matrix_freq.php
+++ matrix_freq.php
@@ -15,18 +15,36 @@ require 'head.php';
 <tr>
     <th class="mdl-data-table__cell--non-numeric">Main Ball Numbers</th>
     <th>Drawn</th>
+    <th>Last Drawn</th>
+    <th>Days Ago</th>
 </tr>
 </thead>
 <tbody>
 <?php
-$qwe2 = file_get_contents("csv/megamillions-adj-main.csv");
+
+$qwe = file_get_contents("csv/megamillions-adj-main.csv");
+$datetime1 = date_create(date('Y-m-d'));
+
 foreach(range(1, 70) as $key => $number) {
 $number = sprintf('%02d', $number);
 
 echo "<tr class='mdl-data-table__cell--non-numeric''>";
 if( is_numeric($key) ) {
     echo "<td class='main_nums'>".$number."</td>";
-    echo "<td class='rowone'>".substr_count($qwe2,$number)."</td>";
+    echo "<td class='rowone'>".substr_count($qwe,$number)."</td>";
+
+    $ch = fopen("csv/megamillions-adj.csv", "r");
+    while($row = fgetcsv($ch)) {
+        if (preg_match('/'.$number.'/', $row[1])) {
+            echo '<td>' . $row[0] . ' </td>';
+            break;
+        }
+    }
+    fclose($ch);
+    
+    $datetime2 = date_create($row[0]);
+    $interval = date_diff($datetime1, $datetime2);
+    echo "<td>".$interval->format('%a')."</td>";
     }
 
 echo "</tr>";
@@ -40,18 +58,36 @@ echo "</tr>";
 <tr>
     <th class="mdl-data-table__cell--non-numeric">Mega Ball Numbers</th>
     <th>Drawn</th>
+    <th>Last Drawn</th>
+    <th>Days Ago</th>
 </tr>
 </thead>
 <tbody>
 <?php
-$qwe2 = file_get_contents("csv/megamillions-adj-mega.csv");
+
+$qwe = file_get_contents("csv/megamillions-adj-mega.csv");
+$datetime1 = date_create(date('Y-m-d'));
+
 foreach(range(1, 25) as $key => $number) {
 $number = sprintf('%02d', $number);
 
 echo "<tr class='mdl-data-table__cell--non-numeric''>";
 if( is_numeric($key) ) {
     echo "<td class='mega_ball'>".$number."</td>";
-    echo "<td class='rowone'>".substr_count($qwe2,$number)."</td>";
+    echo "<td class='rowone'>".substr_count($qwe,$number)."</td>";
+
+    $ch = fopen("csv/megamillions-adj.csv", "r");
+    while($row = fgetcsv($ch)) {
+        if (preg_match('/'.$number.'/', $row[2])) {
+            echo '<td>' . $row[0] . ' </td>';
+            break;
+        }
+    }
+    fclose($ch);
+    
+    $datetime2 = date_create($row[0]);
+    $interval = date_diff($datetime1, $datetime2);
+    echo "<td>".$interval->format('%a')."</td>";
     }
 
 echo "</tr>";
--- megamillions.py
+++ megamillions.py
@@ -15,8 +15,17 @@ df['Mega Ball'] = df['Mega Ball'].apply('{:02d}'.format)
 df.to_csv('p-a-t-h/csv/megamillions.csv', mode='w')
 
 
+#Adjusted
+df = pd.read_csv('p-a-t-h/csv/megamillions.csv')
+
+df['Mega Ball'] = df['Mega Ball'].apply('{:02d}'.format)
+df['Multiplier'] = df['Multiplier'].astype('Int64')
+df = df[pd.to_datetime(df['Draw Date']) > '2017-10-31']
+df.to_csv('p-a-t-h/csv/megamillions-adj.csv', mode='w', index=None)
+
+
 #Mega_Millions Clean
-df = pd.read_csv('p-a-t-h/csv/megamillions.csv',
+df = pd.read_csv('p-a-t-h/csv/megamillions-adj.csv',
             index_col=False,
             header=0,
             usecols = [1, 2],
@@ -24,17 +33,24 @@ df = pd.read_csv('p-a-t-h/csv/megamillions.csv',
             )
 
 df['Mega Ball'] = df['Mega Ball'].apply('{:02d}'.format)
-df['Winning Numbers'] = df['Winning Numbers'].astype(str) + ' ' + df['Mega Ball'].astype(str)
 df.drop('Mega Ball', inplace=True, axis=1)
 df.to_csv('p-a-t-h/csv/megamillions_clean.csv', header=None, index=None)
 
+df = pd.read_csv('p-a-t-h/csv/megamillions-adj.csv',
+            index_col=False,
+            header=0,
+            usecols = [1, 2],
+            names=['Draw Date', 'Winning Numbers', 'Mega Ball', 'Multiplier'])
+
+df['Mega Ball'] = df['Mega Ball'].apply('{:02d}'.format)
+df['Winning Numbers'] = df['Winning Numbers'].astype(str) + ' ' + df['Mega Ball'].astype(str)
+df.drop('Mega Ball', inplace=True, axis=1)
+df.to_csv('p-a-t-h/csv/megamillions_days_clean.csv', header=None, index=None)
+
 
 #Tuesday
-df = pd.read_csv('p-a-t-h/csv/megamillions.csv')
-df['Multiplier'] = df['Multiplier'].astype('Int64')
-
+df = pd.read_csv('p-a-t-h/csv/megamillions-adj.csv')
 df = df[pd.to_datetime(df['Draw Date']).dt.weekday == 1]
-
 df.to_csv('p-a-t-h/csv/megamillions_tuesday.csv', mode='w', index=None)
 
 df = pd.read_csv('p-a-t-h/csv/megamillions_tuesday.csv',
@@ -50,11 +66,8 @@ df.to_csv('p-a-t-h/csv/megamillions_tuesday_clean.csv', header=None, index=None)
 
 
 #Friday
-df = pd.read_csv('p-a-t-h/csv/megamillions.csv')
-df['Multiplier'] = df['Multiplier'].astype('Int64')
-
+df = pd.read_csv('p-a-t-h/csv/megamillions-adj.csv')
 df = df[pd.to_datetime(df['Draw Date']).dt.weekday == 4]
-
 df.to_csv('p-a-t-h/csv/megamillions_friday.csv', mode='w', index=None)
 
 df = pd.read_csv('p-a-t-h/csv/megamillions_friday.csv',
@@ -67,3 +80,27 @@ df['Mega Ball'] = df['Mega Ball'].apply('{:02d}'.format)
 df['Winning Numbers'] = df['Winning Numbers'].astype(str) + ' ' + df['Mega Ball'].astype(str)
 df.drop('Mega Ball', inplace=True, axis=1)
 df.to_csv('p-a-t-h/csv/megamillions_friday_clean.csv', header=None, index=None)
+
+
+#Main Ball Numbers Only
+df = pd.read_csv('p-a-t-h/csv/megamillions-adj.csv',
+            index_col=False,
+            header=0,
+            usecols = [1, 2],
+            names=['Draw Date', 'Winning Numbers', 'Mega Ball', 'Multiplier'])
+
+df['Mega Ball'] = df['Mega Ball'].apply('{:02d}'.format)
+df.drop('Mega Ball', inplace=True, axis=1)
+df.to_csv('p-a-t-h/csv/megamillions-adj-main.csv', header=None, index=None)
+
+
+#Mega Ball Numbers Only
+df = pd.read_csv('p-a-t-h/csv/megamillions-adj.csv',
+            index_col=False,
+            header=0,
+            usecols = [1, 2],
+            names=['Draw Date', 'Winning Numbers', 'Mega Ball', 'Multiplier'])
+            
+df['Mega Ball'] = df['Mega Ball'].apply('{:02d}'.format)
+df.drop('Winning Numbers', inplace=True, axis=1)
+df.to_csv('p-a-t-h/csv/megamillions-adj-mega.csv', header=None, index=None)
